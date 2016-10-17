fee1e201d832e69f80fa038635e62ce6413a7c4f
anaelChardan <anael.chardan@gmail.com>
2016-10-18 00:35:21 +0200

    Add a repository

---
+++ src/main/java/fr/mines/event_manager/event/repository/EventRepository.java
@@ -0,0 +1,8 @@
+package fr.mines.event_manager.event.repository;
+
+import fr.mines.event_manager.event.entity.Event;
+import fr.mines.event_manager.global.repository.CRUDManager;
+
+public class EventRepository extends CRUDManager<Event> {
+
+}
