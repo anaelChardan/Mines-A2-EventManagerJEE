a50eac5cd6afacc6f7e58e23c3ff51db0a6f7043
Damien <renauddamien86@gmail.com>
2016-10-20 13:39:17 +0200

    Mise en place du processus de connexion

--- src/main/java/fr/mines/event_manager/app/servlet/AppServlet.java
+++ src/main/java/fr/mines/event_manager/app/servlet/AppServlet.java
@@ -4,6 +4,7 @@ import fr.mines.event_manager.framework.servlet.BaseServlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,7 +19,50 @@ public class AppServlet extends BaseServlet {
         }};
     }
 
+    @Override
+    protected Map<String, Pattern> initPostRoutes() {
+        return new HashMap<String, Pattern>()
+        {{
+            put("loginPost", Pattern.compile("/loginpost"));
+        }};
+    }
+
     protected void login(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         this.render("login.jsp", request, response);
+
     }
+
+    protected void loginPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+        System.out.println("TADA");
+        // On récupère les attributs
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String remember = request.getParameter("remember");
+
+
+        HashMap<String, String> mockUsers = new HashMap();
+        mockUsers.put("Damien","test");
+
+        String errorMessage = "";
+        if(mockUsers.containsKey(username)){
+            if(mockUsers.get(username).equals(password)){
+                HttpSession session = request.getSession();
+                session.setAttribute(username,password);
+                this.render("home.jsp",request,response);
+            }
+            else{
+                errorMessage = "Le mot de passe n'est pas valide";
+            }
+        }else{
+            errorMessage = "L'adresse mail et/ou le mot de passe ne sont pas valides";
+        }
+        request.setAttribute("errorMessage",errorMessage);
+        this.render("login.jsp", request, response);
+
+    }
+
+
+
 }
